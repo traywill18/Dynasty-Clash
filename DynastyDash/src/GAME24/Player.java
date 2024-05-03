a64538066bd06de8830695b16c0be5e81b1eda21
Trai Williams <72702402+traywill18@users.noreply.github.com>
2024-05-03 06:53:27 -0400

    Player.java

--- DynastyDash/src/GAME24/Player.java
+++ DynastyDash/src/GAME24/Player.java
@@ -11,7 +11,8 @@ import javax.imageio.ImageIO;
 public class Player extends Character {
 	
 	GameManager gm;
-	InputMapper intmap;  
+	InputMapper intmap;
+	  
 	
 	// adding the constructor 
 
@@ -22,14 +23,15 @@ public class Player extends Character {
 		
 		setDefaultValues();
 		getPlayerImage();
+		boundingBox = new Rect(x-10, y-10, 500, 500); 
 		
 	}
 	// setting the default values for the player 
 	public void setDefaultValues() {
-		x     = 100;
-		y     = 100;
+		x     = 50;
+		y     = 50;
 		speed = 7;
-		direction = "left";
+		direction = "left"; 
 	}
 	
 	public void getPlayerImage() {
@@ -54,43 +56,43 @@ public class Player extends Character {
 	}
 	
 	public void update() {
-		 if(intmap.upPressed == true) {
-			 direction = "up";
-			 y -= speed;
-			 
-		 } else if(intmap.downPressed == true) {
-			 direction = "down";
-			 y += speed;
-			 
-		 }else if(intmap.leftPressed == true) {
-			 direction = "left";
-			 x -= speed;
-			 
-		 }else if(intmap.rightPressed == true) {
-			 direction = "right";
-			 x += speed;
-		 }
-		 }
-	
-	public void draw(Graphics g2) {
-	    BufferedImage image = null;
-	    
-	    switch(direction) {
-	        case "up":
-	            image = (y % 2 == 0) ? up1 : up2; // Alternate between two images for animation
-	            break;
-	        case "down":
-	            image = (y % 2 == 0) ? down1 : down2;
-	            break;
-	        case "left":
-	            image = (x % 2 == 0) ? left1 : left2;
-	            break;
-	        case "right":
-	            image = (x % 2 == 0) ? right1 : right2;
-	            break;
+	    if (intmap.upPressed) {
+	        direction = "up";
+	        y -= speed;
+	    } else if (intmap.downPressed) {
+	        direction = "down";
+	        y += speed;
+	    } else if (intmap.leftPressed) {
+	        direction = "left";
+	        x -= speed;
+	    } else if (intmap.rightPressed) {
+	        direction = "right";
+	        x += speed;
 	    }
-	    
-	    g2.drawImage(image, x, y, 500, 500, null); // Draw the image at player position
-	}
 
+	    // Update player bounding box position
+	    boundingBox.moveBy(x - boundingBox.x, y - boundingBox.y);
+	}
+	  
+	    
+		public void draw(Graphics g2) {
+		    BufferedImage image = null;
+		    
+		    switch(direction) {
+		        case "up":
+		            image = (y % 2 == 0) ? up1 : up2; // Alternate between two images for animation
+		            break;
+		        case "down":
+		            image = (y % 2 == 0) ? down1 : down2;
+		            break;
+		        case "left":
+		            image = (x % 2 == 0) ? left1 : left2;
+		            break;
+		        case "right":
+		            image = (x % 2 == 0) ? right1 : right2;
+		            break;
+		    }  
+		    g2.drawImage(image, x, y, 500, 500, null); 
+		    
+		}
 }
